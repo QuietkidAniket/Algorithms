74cc5067133b552590937a996c56b6d002b9a934
QuietkidAK <aniketkundu12072004@gmail.com>
2024-08-08 08:53:39 +0530

    ADD binary search tree Files

--- demo.cpp
+++ demo.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    string s = "Aniket";
+#include<bits/stdc++.h>
+#define fi(i,n) for(int i =0; i<n; i++)
+#define fi(i,a,n,b) for(int i= a; i < n; i+=b)
+#define print(c) std::cout<<c<<std::endl
+int main()
+{
+    fi(i,0, 10, 2)print(i);
     
-    cout << *(--(s.end())) << '\n';
+    return 0;
 }
